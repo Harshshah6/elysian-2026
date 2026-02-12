91e51a4458a0cad93c84ec0e41016e54e6029564
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2026-02-12 18:29:35 +0530

    modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,21 @@ import { Particles } from "@/components/ui/particles";
 
 const HomePage = () => {
   return (
-    <div className="relative min-h-screen">
-      <HeroBg />
-      <Particles
-        className="absolute inset-0 z-0"
-        quantity={50}
-        ease={100}
-        size={0.001}
-        refresh
-      />
+    <div className="min-h-screen">
+      {/* Hero section */}
+      <div className="relative z-10">
+        <HeroBg />
+        <Particles
+          className="absolute inset-0 z-0"
+          quantity={50}
+          ease={100}
+          size={0.001}
+          refresh
+        />
+      </div>
+
+      {/* Top Bar */}
+      <div className="relative z-50 p-4">Top Bar</div>
     </div>
   );
 };
